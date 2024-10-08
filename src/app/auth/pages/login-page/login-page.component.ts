ddab21375f3cd991f7989d56268044094b8824bd
Andres Monoga <andresmonoga07@gmail.com>
2024-10-08 09:14:57 -0500

    Areglo input

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
+
 import Swal from 'sweetalert2'
 import { Router } from '@angular/router';
 
@@ -18,8 +19,8 @@ export class LoginPageComponent {
   private router      = inject( Router )
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [ Validators.required, Validators.email ]],
-    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    email: ['andresmonoga@google.com', [ Validators.required, Validators.email ]],
+    password: ['123456', [ Validators.required, Validators.minLength(6) ]],
   })
 
 
